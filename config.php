e7b4ac0ccf3b7a12988a76297dcdb13b4e33952d
Luciano <40997819+Lemm-pt@users.noreply.github.com>
2026-03-23 18:24:13 +0000

    Clear MYSQL_PASS in config.php
    Updated MYSQL_PASS to an empty string for security.

--- config.php
+++ config.php
@@ -7,7 +7,7 @@ define('APP_VERSION',     '1.0.0');
 define('MYSQL_SERVER', 'lhcp1142.webapps.net');
 define('MYSQL_DATABASE', 'vl2tjdok_spacet');
 define('MYSQL_USER', 'vl2tjdok_lemm');
-define('MYSQL_PASS', 'Mackyver');
+define('MYSQL_PASS', '');
 define('MYSQL_CHARSET', 'utf8');
 
 
