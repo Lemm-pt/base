4f758230d278628f5fc50cc8cc27fcb0aaf2d80f
Luciano <40997819+Lemm-pt@users.noreply.github.com>
2026-04-10 08:10:08 +0100

    Obfuscate MYSQL server and database credentials
    Updated MYSQL server and database credentials to obfuscate sensitive information.

--- config.php
+++ config.php
@@ -4,8 +4,8 @@ define('APP_NAME',     'Base');
 define('APP_VERSION',     '1.0.0');
 
 // MYSQL
-define('MYSQL_SERVER', 'lhcp1142.webapps.net');
-define('MYSQL_DATABASE', 'vl2tjdok_spacet');
+define('MYSQL_SERVER', '*********');
+define('MYSQL_DATABASE', '********');
 define('MYSQL_USER', 'vl2tjdok_lemm');
 define('MYSQL_PASS', '');
 define('MYSQL_CHARSET', 'utf8');
